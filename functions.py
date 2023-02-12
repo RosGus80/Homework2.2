8cd75f91d5e9ebd6ebfb59f816d81d59e740cb93
rostislavgusev <rosgus80@gmail.com>
2023-02-12 17:59:06 +0300

    com

--- functions.py
+++ functions.py
@@ -2,33 +2,37 @@ import json
 
 
 def students_load():
+    """Загружает информацию из файла студентов в переменную"""
     with open("students.json") as file:
         data = file.read()
         return json.loads(data)
 
 
 def professions_load():
+    """Загружает информацию из файла профессий в переменную"""
     with open("professions.json") as file:
         data = file.read()
         return json.loads(data)
 
 
 def get_student(data, pk):
-        for student in data:
-            if student["pk"] == pk:
-                return student
-        else:
-            print("У нас нет такого студента")
-            quit()
+    """Находит студента по порядковому номеру и возвращает его. data - загруженная в переменную информация из файла студентов"""
+    for student in data:
+        if student["pk"] == pk:
+            return student
+    else:
+        print("У нас нет такого студента")
+        quit()
 
 
 def get_profession(data, title):
-        for profession in data:
-            if profession["title"] == title:
-                return profession
-        else:
-            print("У нас нет такой профессии")
-            quit()
+    """Находит профессию по названию и возвращает ее. data - загруженная в переменную информация из файла профессий"""
+    for profession in data:
+        if profession["title"] == title:
+            return profession
+    else:
+        print("У нас нет такой профессии")
+        quit()
 
 
 def check_fitness(student, profession):
